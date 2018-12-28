6844016085f5e0b1a419e520b76ae7f6f35ed9df
Sumiki Ohsawa <sumikio@gmail.com>
2018-12-28 01:44:14 -0800

    homework2 update

--- algorithms-divide-conquer/homework2/ruby/homework2.rb
+++ algorithms-divide-conquer/homework2/ruby/homework2.rb
@@ -20,60 +20,84 @@ class Homework2
     #  return
     #end
     
-    #p @array
-    #p "---first: #{@array[origin_index..final_index]}"
-    #p "origin_index: #{origin_index}"
-    #p "final_index: #{final_index}"
+    p @array
+    p "---first: #{@array[origin_index..final_index]}"
+    p "origin_index: #{origin_index}"
+    p "final_index: #{final_index}"
     
     pivit_index = @array.index(@array[origin_index..final_index][@pivit_index])
     pivit = @array[origin_index..final_index][@pivit_index]
-    #p "pivit [#{pivit_index}] : #{ pivit }"
+    p "pivit [#{pivit_index}] : #{ pivit }"
     i = origin_index
 
     for j in origin_index..final_index do
       item = @array[j]
       if j == pivit_index
-        i += 1
         next
       end
       
+      p "compare: #{ item }[#{j}] < #{ pivit }[#{pivit_index}]"
       if item < pivit
-        #p "@array[i]:[#{i}] #{@array[i]} <=> @array[j]:[#{j}] #{@array[j]}"
+        if pivit_index == i
+          i += 1
+        end
+        p "@array[i]:[#{i}] #{@array[i]} <=> @array[j]:[#{j}] #{@array[j]}"
         tmp = @array[i]
         @array[i] = @array[j]
         @array[j] = tmp
         i += 1
       end
+      p @array[origin_index..final_index]
       
     end
     
-    if (i - 1) != pivit_index
-      #p "---pivit swap---@array[pivit_index]:[#{pivit_index}] #{@array[pivit_index]} <=> @array[i - 1]:[#{ i - 1 }]#{@array[i - 1]}"
-      tmp = @array[i - 1]
-      @array[i - 1] = @array[pivit_index]
+    replace_index = i
+    next_origin = i
+    if pivit_index < i
+      replace_index -= 1
+    elsif pivit_index > i
+      next_origin += 1
+    end
+
+    p "----before replace------"
+    p "i: #{i}"
+    p "replace_index: #{replace_index}"
+    p "pivit_index: #{pivit_index}"
+    p "next_origin: #{next_origin}"
+    
+
+    if (replace_index) != pivit_index
+      p "---pivit swap---@array[pivit_index]:[#{pivit_index}] #{@array[pivit_index]} <=> @array[i - 1]:[#{ replace_index }]#{@array[replace_index]}"
+      tmp = @array[replace_index]
+      @array[replace_index] = @array[pivit_index]
       @array[pivit_index] = tmp
     end
     
-    if i > origin_index
-      #p "(i - 2):#{(i - 2)}"
-      #p "origin_index: #{origin_index}"
-      if (i - 2) - origin_index >= 1
-        #p "first : #{ origin_index } #{ i - 2  }"
-        quicksort( origin_index, i - 2  )
-      end
-      
-    end
-
-    next_origin = i
-
-    #p "final_index:#{final_index}"
-    #p "next_origin: #{next_origin}"
-    #p final_index - next_origin
     
-    if final_index - next_origin >= 1
-      #p "second : #{ next_origin } #{ final_index  }"
-      quicksort( next_origin, final_index )
+    p @array
+    p "pivit: [#{pivit_index}] = #{ pivit }"
+    p "=To next =#{ final_index - origin_index > 1 }==========="
+    p "origin_index: #{origin_index}"
+    p "i: #{i}"
+    p "first: (i - 2) - origin_index = #{(replace_index - 1) - origin_index >= 1}, ( #{origin_index}, #{replace_index - 1} )"
+    p "final_index:#{final_index}"
+    p "next_origin: #{next_origin}"
+    p "second: final_index - next_origin = #{final_index - next_origin >= 1} ( #{next_origin}, #{final_index} )"
+    p ''
+
+    # 要素が２個の場合は次はない
+    if final_index - origin_index > 1
+      if (replace_index - 1) - origin_index >= 1
+        p "first : #{ origin_index } #{ replace_index - 1  }"
+        quicksort( origin_index, replace_index - 1  )
+      end
+  
+      if final_index - next_origin > 1
+        p "second : #{ next_origin } #{ final_index  }"
+        quicksort( next_origin, final_index )
+      end
     end
+
     
   end
   
